d8802296ab7a0645604dc38ada0d7b32ea06d9cf
Shukri Adams <shukri@playdead.com>
2019-04-25 11:30:29 +0200

    added try catches to route handlers

--- src/routes/default.js
+++ src/routes/default.js
@@ -7,12 +7,19 @@ module.exports = function(app){
      * This is a catch-all route that forces all page views to our single page app
      */
     app.get(/^[^.]+$/, async function (req, res) {
-        let view = handlebars.getView('default'),
+        try {
+            let view = handlebars.getView('default'),
             model = {
                 bundlemode : settings.bundlemode,
                 bundle : settings.bundle
             };
 
-        res.send(view(model));
+            res.send(view(model));
+
+        } catch(ex){
+            console.log(ex);
+            res.status(500);
+            res.end(ex.toString());
+        }
     });
 }
