e42f337756ee3f11c16a8f46ece6965bd53dea9f
Shukri Adams <shukri@playdead.com>
2019-04-26 11:48:49 +0200

    added missing concat-cli

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
-#cp ./src/client/lib/system.js ./src/public/scripts
-#cp ./src/client/app/config.js ./src/public/scripts
-
+# set up work directories
 mkdir -p ./.tmp/scripts
 mkdir -p ./.tmp/bundles
 
+
+# ./.tmp/scripts is where we stage all files which will be bundles. 
+# This means system.js (which is needed to load everything), config.js(jspm's config)
+# and finally app.js, which is all the scripts from our actual app
 cp ./src/client/lib/system.js ./.tmp/scripts
 cp ./src/client/app/config.js ./.tmp/scripts
 
-
 cd ./src/client/app/
-#jspm bundle-sfx main/main.js ./../../public/scripts/bundle.js
-jspm bundle-sfx main/main.js ./../../../.tmp/scripts/bundle.js
+jspm bundle-sfx main/main.js ./../../../.tmp/scripts/app.js
 cd -
 
-concat-cli -f ./.tmp/scripts/*.js -o ./.tmp/bundles/bundle.js
 
+# bundle all stages scripts into our final bundle, then minify it with uglify
+concat-cli -f ./.tmp/scripts/*.js -o ./.tmp/bundles/bundle.js
 uglifyjs -c -v -o ./.tmp/bundles/bundle.min.js -- ./.tmp/bundles/bundle.js
+
+
+# finally, move the minifies and unminified script to it's final deploy spot in ./src/public/scripts,
 cp ./.tmp/bundles/* ./src/public/scripts
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -11,6 +11,7 @@ sudo apt-get install nodejs -y
 sudo npm install jspm -g
 sudo npm install yarn -g
 sudo npm install uglify-es -g
+sudo npm install concat-cli -g
 
 # force startup folder to vagrant project
 echo "cd /vagrant/src" >> /home/vagrant/.bashrc
