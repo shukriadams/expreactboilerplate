1493e0f3604863006c57d46596c280c01dc53cf2
Shukri Adams <shukri.adams@gmail.com>
2019-05-08 15:35:49 +0200

    added history helper

---
+++ src/client/app/helpers/history.js
@@ -0,0 +1,10 @@
+/**
+ * Single wrapper for history object
+ * Use:
+ *
+ * import history from './history';
+ * history.push('myurl/myParamenters')
+ */
+ 
+import createBrowserHistory from 'history/createBrowserHistory'
+export default createBrowserHistory();
